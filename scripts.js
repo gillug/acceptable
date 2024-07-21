bff38e8d1c14adc5ad44b1863d0b995b8f70a506
Arvind Singh Shekhawat <gillubaba@yahoo.co.in>
2024-07-21 22:34:04 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,73 +1,72 @@
 document.addEventListener('DOMContentLoaded', () => {
-    handleCookieConsent();
-    handleSocialLinksConsent();
+  handleCookieConsent();
+  handleSocialLinksConsent();
 });
 
 function handleCookieConsent() {
-    const cookieConsent = document.getElementById('cookie-consent');
-    const acceptCookies = document.getElementById('accept-cookies');
+  const cookieConsent = document.getElementById('cookie-consent');
+  const acceptCookies = document.getElementById('accept-cookies');
 
-    if (!localStorage.getItem('cookiesAccepted')) {
-        cookieConsent.style.display = 'block';
-    }
+  if (!localStorage.getItem('cookiesAccepted')) {
+    cookieConsent.style.display = 'block';
+  }
 
-    acceptCookies.addEventListener('click', () => {
-        localStorage.setItem('cookiesAccepted', 'true');
-        cookieConsent.style.display = 'none';
-    });
+  acceptCookies.addEventListener('click', () => {
+    localStorage.setItem('cookiesAccepted', 'true');
+    cookieConsent.style.display = 'none';
+  });
 }
 
 function handleSocialLinksConsent() {
-    const socialLinks = document.querySelectorAll('.social-link');
-    const consentPopup = document.getElementById('consent-popup');
-    const consentPlatform = document.getElementById('consent-platform');
-    const consentYes = document.getElementById('consent-yes');
-    const consentNo = document.getElementById('consent-no');
+  const socialLinks = document.querySelectorAll('.social-link');
+  const consentPopup = document.getElementById('consent-popup');
+  const consentPlatform = document.getElementById('consent-platform');
+  const consentYes = document.getElementById('consent-yes');
+  const consentNo = document.getElementById('consent-no');
 
-    socialLinks.forEach(link => {
-        link.addEventListener('click', event => {
-            event.preventDefault(); // Prevent default action
-            const targetHref = link.href;
-            const targetDeeplink = link.getAttribute('data-deeplink');
-            const platform = link.dataset.platform;
-            consentPlatform.textContent = platform;
-            consentPopup.style.display = 'flex';
+  socialLinks.forEach((link) => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      const targetHref = link.href;
+      const targetDeeplink = link.getAttribute('data-deeplink');
+      const platform = link.dataset.platform;
 
-            consentYes.addEventListener('click', () => {
-                consentPopup.style.display = 'none';
-                deepLink(link);
-            }, { once: true }); // Ensure the event listener is called only once
+      consentPlatform.textContent = platform;
+      consentPopup.style.display = 'flex';
 
-            consentNo.addEventListener('click', () => {
-                consentPopup.style.display = 'none';
-            }, { once: true }); // Ensure the event listener is called only once
-        });
+      consentYes.addEventListener('click', () => {
+        consentPopup.style.display = 'none';
+        deepLink(link);
+      }, { once: true });
+
+      consentNo.addEventListener('click', () => {
+        consentPopup.style.display = 'none';
+      }, { once: true });
     });
+  });
 }
 
 function deepLink(element) {
-    const deeplink = element.getAttribute('data-deeplink');
-    const androidUrl = element.getAttribute('data-android-url');
-    const iosUrl = element.getAttribute('data-ios-url');
-    const webUrl = element.getAttribute('data-web-url');
-    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+  const deeplink = element.getAttribute('data-deeplink');
+  const androidUrl = element.getAttribute('data-android-url');
+  const iosUrl = element.getAttribute('data-ios-url');
+  const webUrl = element.getAttribute('data-web-url');
+  const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+  let url;
 
-    let url;
-    if (/android/i.test(userAgent)) {
-        url = androidUrl;
-    } else if (/iPad|iPhone|iPod/.test(userAgent) || !window.MSStream) {
-        url = iosUrl;
-    } else {
-        url = webUrl;
+  if (/android/i.test(userAgent)) {
+    url = androidUrl;
+  } else if (/iPad|iPhone|iPod/.test(userAgent) || !window.MSStream) {
+    url = iosUrl;
+  } else {
+    url = webUrl;
+  }
+
+  window.location.href = deeplink || url;
+
+  setTimeout(() => {
+    if (deeplink && !document.hasFocus()) {
+      window.location.href = webUrl;
     }
-
-    // Attempt to open the deep link URL
-    window.location.href = deeplink || url;
-
-    // If the app is not opened within 1 second, redirect to the web URL
-    setTimeout(() => {
-        if (deeplink || !document.hasFocus()) {
-            window.location.href = webUrl;
-        }
-    }, 1000);
+  }, 1000);
 }
