63e1982d762b9254a2ba1dde2f4b6e2f8a5719dc
Arvind Singh Shekhawat <gillubaba@yahoo.co.in>
2024-07-21 16:26:30 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,15 +1,46 @@
-function deepLink(element) {
-    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-    var urlParams = new URLSearchParams(window.location.search);
-    var androidUrl = urlParams.get('android');
-    var iosUrl = urlParams.get('ios');
-    var webUrl = urlParams.get('web');
+<script type='text/javascript' src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+        
+    <script type='text/javascript'>
+        document.querySelectorAll('.link-list a').forEach(link => {
+            link.addEventListener('click', function(event) {
+                event.preventDefault();
 
-    if (/android/i.test(userAgent)) {
-        window.location = androidUrl;
-    } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-        window.location = iosUrl;
-    } else {
-        window.location = webUrl;
-    }
-}
+                const platform = getPlatform();
+                const androidUrl = this.getAttribute('data-android-url');
+                const iosUrl = this.getAttribute('data-ios-url');
+                const webUrl = this.getAttribute('data-web-url');
+
+                let appUrl;
+                if (platform === 'android') {
+                    appUrl = androidUrl;
+                } else if (platform === 'ios') {
+                    appUrl = iosUrl;
+                } else {
+                    window.location = webUrl;
+                    return;
+                }
+
+                // Try to open the app URL
+                window.location = appUrl;
+
+                // If the app is not opened within 1 second, redirect to the web URL
+                setTimeout(() => {
+                    window.location = webUrl;
+                }, 1000);
+            });
+        });
+
+        function getPlatform() {
+            const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+            if (/android/i.test(userAgent)) {
+                return 'android';
+            }
+
+            if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+                return 'ios';
+            }
+
+            return 'web';
+        }
+    </script>
